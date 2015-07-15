a46f09ffbfd38e3daddcf96bbfaa02aafe60b8f1
devitito <the.devitito@gmail.com>
2015-07-15 16:52:59 +0200

    basic ionic config

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,57 @@
 
-angular.module('mindmeteo', ['ionic', 'LocalStorageModule'])
+angular.module('mindmeteo', ['ionic', 'LocalStorageModule', 'dashboard'])
 
-.config(['$stateProvider', 'localStorageServiceProvider', '$urlRouterProvider', function($stateProvider, localStorageServiceProvider, $urlRouterProvider) {
+.config(['$stateProvider',
+         'localStorageServiceProvider',
+         '$urlRouterProvider',
+         '$ionicAppProvider', function($stateProvider, localStorageServiceProvider, $urlRouterProvider, $ionicAppProvider) {
 
-    $stateProvider
-    .state('index', {
-        url: '/',
-        template: '<h1>Mindmeteo</h1>'
+    //Ionic app config
+    $ionicAppProvider.identify({
+        app_id: '1167016d',
+        api_key: 'cae419de70fc9fbf4e9bb89e31287f18c5ec3502ee2d4723',
+        // The GCM project ID (project number) from your Google Developer Console (un-comment if used)
+        // gcm_id: 'YOUR_GCM_ID'
     });
 
-    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('dashboard', {
+        url: '/dashboard',
+        abstract: true,
+        templateUrl: 'templates/dashboard.html'
+    })
+
+    .state('dashboard.climate', {
+        url: '/climate',
+        views: {
+          'dash-climate': {
+            templateUrl: 'templates/dash-climate.html',
+            controller: 'dashClimateCtrl'
+          }
+        }
+    })
+
+    .state('dashboard.reports', {
+        url: '/reports',
+        views: {
+          'dash-reports': {
+            templateUrl: 'templates/dash-reports.html',
+            controller: 'dashReportsCtrl'
+          }
+        }
+    })
+
+    /*.state('dashboard.profile', {
+        url: '/profile',
+        views: {
+          'dash-profile': {
+            templateUrl: 'templates/dash-profile.html',
+            controller: 'dashProfileCtrl'
+          }
+        }
+    });*/
+
+    $urlRouterProvider.otherwise('/dashboard/climate');
 
     localStorageServiceProvider
         .setStorageType('sessionStorage')
@@ -20,7 +62,7 @@ angular.module('mindmeteo', ['ionic', 'LocalStorageModule'])
 
 .run(['$ionicPlatform', '$http', function($ionicPlatform, $http) {
     $ionicPlatform.ready(function() {
-        $http.get('http://localhost:1337/csrfToken').success(function(data){
+        $http.get('/csrfToken').success(function(data){
             $http.defaults.headers.common['x-csrf-token'] = data._csrf;
         });
     });
---
+++ www/js/dashboard.js
@@ -0,0 +1,9 @@
+
+
+angular.module('dashboard', [])
+.controller('dashClimateCtrl', ['$scope', function($scope) {
+}])
+.controller('dashReportsCtrl', ['$scope', function($scope) {
+}])
+.controller('dashProfileCtrl', ['$scope', function($scope) {
+}]);
